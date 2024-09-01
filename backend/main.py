5e8d40bf4c310c9bc515baa0930100670bfd4857
Logan Miller <millerl1960@gmail.com>
2024-09-01 10:12:04 -0700

    Moves main out of app folder

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from api.endpoints import router
+from app.api.endpoints import router
 import os
 
 app = FastAPI()
